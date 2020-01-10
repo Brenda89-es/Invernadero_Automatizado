6872a898140c084aff28ec9075344590467416d0
Brenda89-es <40180315+Brenda89-es@users.noreply.github.com>
2020-01-10 21:08:26 +0100

    Activación del riego
    Programa escrito en código python  para ser ejecutado en la Raspberry. Se activa/desactiva el riego controlado por un relé cuyo comportamiento depende de un sensor de humedad de suelo.

---
+++ activacion-riego.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+#Programa que mide la humedad del suelo y activa o desactiva el riego en función de la misma
+
+#Librerías
+from gpiozero import DigitalInputDevice
+import RPi.GPIO as GPIO
+import time
+
+#Métodos
+GPIO.setmode(GPIO.BCM) #Nos referimos a los pines por su número de Broadcomm SOC Channel.
+GPIO.setwarnings(False) #Método para ocultar avisos
+
+#Relé conectado al GPIO27
+GPIO.setup(27, GPIO.OUT)
+GPIO.output(27, GPIO.HIGH)
+
+#Higrómetro conectado al GPIO17
+d0_input = DigitalInputDevice(17) 
+
+#Función principal
+while True:
+    
+    if (not d0_input.value):
+        GPIO.output(27, GPIO.LOW)
+        print("Riego desactivado")
+        time.sleep(2)
+    else:
+        GPIO.output(27, GPIO.HIGH)
+        print("Riego activado")
+        time.sleep(2)
+
