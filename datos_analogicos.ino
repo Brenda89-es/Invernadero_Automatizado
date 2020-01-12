b08a4340442be2b883ace538569f5c9120a6b484
Brenda89-es <40180315+Brenda89-es@users.noreply.github.com>
2020-01-12 20:55:50 +0100

    Update datos_analogicos.ino

--- datos_analogicos.ino
+++ datos_analogicos.ino
@@ -1,15 +1,15 @@
 /*Programa que toma valores analogicos de temperatura, humedad, presion, luminosidad, humedad del suelo*/
 
-//Librerias necesarias para el sensor BME/BME280
+//Librerias necesarias para el sensor BME/BME280.
 # include <Wire.h>
 # include <Adafruit_Sensor.h>
 # include <Adafruit_BME280.h>
 
 
-//Declaracion de variables necesarias y su pin analogico correspondiente
+//Declaracion de variables necesarias y su pin analogico correspondiente.
 # define BME_SDA A4
 # define BME_SCL A5
-# define SEALEVELPRESSURE_HPA (1013.25) #Valor del presion
+# define SEALEVELPRESSURE_HPA (1013.25) //Valor de la presion.
 
 Adafruit_BME280 bme;
 
@@ -21,22 +21,22 @@ int valor_luminosidad;
 void setup() 
 {
     Serial.begin(9600);  
-    bme.begin(0x76);   //Jumper,valor hexadecimal que establecemos
+    bme.begin(0x76);   //Jumper,valor hexadecimal que establecemos.
     contador = 1; //Contador desde el valor 1, no desde 0. 
     delay(2000);  
 }
 
 void loop() 
 {
-    //Declaracion de variables 
+    //Declaracion de variables. 
     float Temperatura, Humedad, Presion;
     
     //Lectura
     Temperatura = bme.readTemperature();
     Humedad = bme.readHumidity();
     Presion = bme.readPressure();   
-    humedad = map(analogRead(A3),1023, 0, 0, 100); //Mapeo de la variable para que los valores vayan de 0 a 100
-    valor_luminosidad = map(analogRead(A2), 1023, 0 , 0, 100); //Mapeo de la variable, valores de 0 a 100
+    humedad = map(analogRead(A3),1023, 0, 0, 100); //Mapeo de la variable para que los valores vayan de 0 a 100.
+    valor_luminosidad = map(analogRead(A2), 1023, 0 , 0, 100); //Mapeo de la variable, valores de 0 a 100.
 
     //Salidad
     Serial.print(contador); Serial.print("\t"); 
