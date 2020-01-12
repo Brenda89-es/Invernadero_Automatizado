35621dc17820e3d56dcd9d45002ae44d394b6aaa
Brenda89-es <40180315+Brenda89-es@users.noreply.github.com>
2020-01-12 20:58:17 +0100

    Update activacion-riego.py

--- activacion-riego.py
+++ activacion-riego.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
 
-#Programa que mide la humedad del suelo y activa o desactiva el riego en función de la misma
+#Programa que mide la humedad del suelo y activa o desactiva el riego en función de la misma.
 
-#Librerías
+#Librerías.
 from gpiozero import DigitalInputDevice
 import RPi.GPIO as GPIO
 import time
 
 #Métodos
-GPIO.setmode(GPIO.BCM) #Nos referimos a los pines por su número de Broadcomm SOC Channel
-GPIO.setwarnings(False) #Método para ocultar avisos
+GPIO.setmode(GPIO.BCM) #Nos referimos a los pines por su número de Broadcomm SOC Channel.
+GPIO.setwarnings(False) #Método para ocultar avisos.
 
-#Relé conectado al GPIO27
+#Relé conectado al GPIO27.
 GPIO.setup(27, GPIO.OUT)
 GPIO.output(27, GPIO.HIGH)
 
-#Higrómetro conectado al GPIO17
+#Higrómetro conectado al GPIO17.
 d0_input = DigitalInputDevice(17) 
 
-#Función principal
+#Función principal.
 while True:
     
     if (not d0_input.value):
-        GPIO.output(27, GPIO.LOW) #Relé apagado, se desactiva el riego
+        GPIO.output(27, GPIO.LOW) #Relé apagado, se desactiva el riego.
         print("Riego desactivado")
         time.sleep(2)
     else:
-        GPIO.output(27, GPIO.HIGH) #Relé encendido, se activa el riego
+        GPIO.output(27, GPIO.HIGH) #Relé encendido, se activa el riego.
         print("Riego activado")
         time.sleep(2)
 
